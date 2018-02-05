880b3ed3b5ce1ea819c44ac2dc62a130834813c1
grajauskas <e.grajauskas@gmail.com>
2018-02-05 09:34:39 +0200

    pull error - before merge

---
+++ uzduotys/diena_10/UZDUOTYS/uzduotys - more js/uzduotis-js-ARRAY-6.js
@@ -0,0 +1,39 @@
+var masinosBagazine = ["atsarginisRatas", "kastuvas", "gesintuvas", "virve", "uzkurimoLaidai", "krokodilas"];
+
+console.warn("console blogai atvaizduoja isskleistus masyvus, bet gerai atvaizduoja skliaustuose []");
+
+
+// =====sukurti masyva 100 elementu ilgio naudojant FOR
+//===== sukurti masyva 100 elementu ilgio naudojant WHILE
+
+
+// ————————————pasikartojimas f-ju ir while————————————————————————
+
+// Uzduotis:
+// 1A) susikurti funkcija, kuri atspasudina i konsole skaiciu kuri paduodate
+// 1B) Paleisti funkcija 20 kartu
+// 2) susikurti funkcija, kuri spausdina paduota skaiciu, kai paduodama reiksme (jeigu niekas nepaduodama: spausdina "-1")
+
+//------------while break-- uzduotis:----------------------------
+
+
+//  sukurti nauja masyva uzpildyta, atsitiktiniais skaiciais
+//  var nuoVienoIkiSimto = Math.floor(Math.random() * 100) ;
+skaiciai = [];
+console.log(skaiciai.toString());
+
+
+
+
+
+
+// ——————————— Object ————————————————
+// Uzduotis
+// 1:  susiskurti darbuotojo "worker" obj., kuris saugo varda, pavarde, amziu, pagamintu detaliu skaiciu
+// 2. sukurti “workers”  masyva. Masyvas, kuriame saugomi objektai. Kiekvienas obj saugo visa informacija apie konkretu darbuotoja:
+    //    A) paleisti cikla ir uzpildyti "workers" masyva darbuotoju duomenimis (amzius ir pagamintu detaliu skaicius - burtu keliu)
+//	HINT: // !!!!! var naudojame, kad RAM'e / atmintyje sukurtu nauja kintamaji
+// 3. sukurti f-ja, kuri i konsole atspausdiname masyva (pilna objektu)
+
+
+ 

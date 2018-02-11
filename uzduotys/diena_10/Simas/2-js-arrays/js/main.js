4bfdbcaf5cac8e9121f92933be538c981d60b6ea
Simas Veleckas <34476626+SimasVe@users.noreply.github.com>
2018-02-11 18:40:37 +0200

    namu_darbai

--- uzduotys/diena_10/Simas/2-js-arrays/js/main.js
+++ uzduotys/diena_10/Simas/2-js-arrays/js/main.js
@@ -90,3 +90,16 @@ ieskomiZmones = [2, 16, 17, 18, 19, 25];
 var names = ["Enriqueta", "Sybil", "Piper", "Anh", "Carmelo", "Regan", "Synthia", "Newton", "Norbert", "Krystyna", "Fidelia", "Christoper", "Lewis", "Jeromy", "Joy", "Lorri", "Owen", "Rosenda", "Devora", "Treva", "Leanora", "Meghann", "Jacqueline", "Bunny", "Tenisha", "Rico", "Clementina", "Samella", "Rico", "Sandi", "Efrain", "Tena", "Vivan", "Hiedi", "Naida", "Evette", "Shane", "Freida", "Marielle", "Wynona", "Cheree", "Gaston", "Aja", "Timika", "Jude", "Griselda", "Luise", "Rico", "Minh", "Warren"];
 
 var lastNames = ["Mcdowell", "Gates", "Mccall", "Cisneros", "Hancock", "Gaines", "Juarez", "Nolan", "Barajas", "Ware", "Orr", "Bell", "Donovan", "Rojas", "Stevenson", "Long", "Hays", "Gibson", "Meyer", "Sims", "Mcintosh", "Craig", "Haney", "Cunningham", "Hunt", "Montgomery", "Spears", "Cooke", "Gregory", "Mcknight", "Fernandez", "Hendrix", "Patton", "Bond", "Skinner", "Randolph", "Montes", "Guerra", "Bowen", "Potts", "Dyer", "Riley", "Rodgers", "Schroeder", "Ferguson", "Garrett", "Rush", "Moon", "Whitney", "Mcdaniel"];
+
+
+// rimta
+// 1 kursime nauja masyva uzpildyta, atsitiktiniais skaiciais nuo 50 - 200 (pagamintomis datalemis per diena) 
+
+function atiktiniuSkArray (apimtis, minSk, maxSk) {
+    var atsitiktineArray = [];
+    for (let i = 0; i < apimtis; i++) {
+        var atsitiktinisSkaicius = Math.round(Math.random() * (maxSk - minSk) + minSk);
+        atsitiktineArray[i] = atsitiktinisSkaicius;
+    }
+    return atsitiktineArray;
+}

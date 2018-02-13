52a9cc16fefb7522850811702be4ebb3e085658c
grajauskas <e.grajauskas@gmail.com>
2018-02-13 16:48:24 +0200

    12.1

--- uzduotys/MAMP/diena_12/DESTYTOJAS/1-php-basics/index.php
+++ uzduotys/MAMP/diena_12/DESTYTOJAS/1-php-basics/index.php
@@ -37,7 +37,7 @@
 
         //---------------local---------------
         $vardas = "Tomas";
-        function manoFunkcija( $x ) { // $x (local)- naujas issigalvotas laikinas kintamamasis
+        function <manoFunkcija></manoFunkcija>( $x ) { // $x (local)- naujas issigalvotas laikinas kintamamasis
             echo "1. x pries pakeiciant:" .  $x . "<br>";
             $x = "Karolis Didysis";
         }
@@ -46,7 +46,7 @@
         echo $vardas . "<br>";
         // pasikartojimas return
         // -------------return---------------
-        function getSkaicius() {
+        function <getSkaicius></getSkaicius>() {
             return 10;
         }
         echo "Mano skaicius: " . getSkaicius();
@@ -68,6 +68,9 @@
          setManoFunkcija( $pavarde );
          echo "Pavarde (po pakeitimo) yra: $pavarde <br>";
 
+
+         define( 'SLAPTAZODIS', 'XDAka152a.');
+         echo SLAPTAZODIS . "<br />";
         ?>
 
         <!-- js puslapio apacioje -->

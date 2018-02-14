5037678f4183bc91bcf3f9c65b89ef418c204736
grajauskas <e.grajauskas@gmail.com>
2018-02-14 10:20:59 +0200

    13.1

---
+++ uzduotys/MAMP/diena_13/DESTYTOJAS/1-php-array/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+
+        <!-- !!! mano CSS failas vissada zemiau, nie kiti css failai -->
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="./libs/bootstrap/css/bootstrap.css">
+
+        <!-- !!idedame CSS faila, nes kitaip neveiks -->
+        <link rel="stylesheet" href="./css/main.css">
+        <!-- NOTE: jeigu neveikia:
+            1) ar failo pavadinimas geras
+            2) ar kelias geras?   (ar "/" teisingi; direktorija ar gere )
+            3) ar   rel="stylesheet"
+            4) ar link uzdalete ">"
+         -->
+    </head>
+    <body>
+
+        <h1>Isijunk konsole</h1>
+
+        <?php
+
+        // uzduotis 1
+        // A. susikurti meniu masyva: home, about, gallery, contact, blog
+        // B. atspausdinti visus meniu punktus i HTML (su html tag'ais)
+        $manoMenu = ["home", "about", "gallery", "contact", "blog"];
+        // print_r( $manoMenu ); // pasitestuojame
+        ?>
+
+        <nav >
+            <ul class="nav navbar">
+                <li class="nav-item">
+                    <?php echo $manoMenu[0]; ?>
+                </li>
+                <li class="nav-item">
+                    <?php echo $manoMenu[1]; ?>
+                </li>
+                <li class="nav-item">
+                    <?php echo $manoMenu[2]; ?>
+                </li>
+                <li class="nav-item">
+                    <?php echo $manoMenu[3]; ?>
+                </li>
+                <li class="nav-item">
+                    <?php echo $manoMenu[4]; ?>
+                </li>
+            </ul>
+        </nav>
+
+
+          <!-- ========menu su FOR===== -->
+          <!-- // echo  "<li class=\"nav-item\">" ;  // "\" ignoruoja viena, sekanti simboli -->
+         <!-- // O kaip atspausdinti "\\" ?   echo "\\\\"; -atspausdins \\ -->
+          <nav >
+              <ul class="nav navbar">
+                 <?php
+                    for ($i=0; $i < count( $manoMenu ); $i++) {
+                            // echo  "<li class='nav-item'>" ;
+                            //             echo $manoMenu[$i];
+                            // echo '</li>';
+                            // ARBA
+                            echo  "<li class='nav-item'>" . $manoMenu[$i] . '</li>';
+                    }
+                  ?>
+              </ul>
+          </nav>
+        <?php
+        // uzduotis 2
+        // A. susirasti 6 nuotraukas
+        // B. susikurti nuotrauku pavadinimu masyva
+        // C. atspausdinti visas nuotraukas su ciklu, po 3-ris i eilute
+        // <img src='./img/1.jpg' alt=''  />
+
+
+
+        // uzduotis 3
+        // sukurti matrica, kurioje bus saugoma kiekvienos prekes info:
+        //  Antraste, img pavadinimas, prekes aprasymas, kaina
+        // A. sukurti masyva preke: Antraste, img pavadinimas, prekes aprasymas, kaina
+        // B. i masyva visosPrekes, ideti "preke" masyva
+        // C. atvaizduodi kiekvienoje eiluteje po 3 prekes (naudojant for arba foreach)
+
+
+           // 3 budai
+            // printf("<img src='./img/" . $visosPrekes[$i][1] . "' width='200px;' alt=''  />");
+            // printf("<img src='./img/%s' width='200px;' alt=''  />", $visosPrekes[$i][1]);
+
+
+
+
+         ?>
+
+
+
+
+
+
+
+
+
+        <!-- js puslapio apacioje -->
+        <script src="http://code.jquery.com/jquery-3.3.1.min.js"  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <!-- !!! mano js failas - VISADA pats zemiausias -->
+        <script type="text/javascript" src="js/main.js">     </script>
+    </body>
+</html>

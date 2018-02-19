125b5444997bb5cffbe747ba2cc404aea80b0669
grajauskas <e.grajauskas@gmail.com>
2018-02-19 14:39:28 +0200

    db

--- uzduotys/MAMP/diena_15/DESTYTOJAS/1-doctors-database/doctors_db.php
+++ uzduotys/MAMP/diena_15/DESTYTOJAS/1-doctors-database/doctors_db.php
@@ -12,19 +12,41 @@
 // C:\MAMP\bin\mysql\bin\mysql.exe -u root -p 'hospital1' < "C:\Users\User\Documents\Projektas\uzduotys\MAMP\diena_15\mano_DB.sql"
 
  define('DB_NAME', 'hospital1');
- define('UNSER', 'root------- ');
+ define('UNSER', 'root');
  define('PASSWORD', 'root');
  define('DB_HOST', 'localhost');
 
- $connnection = mysqli_connect( DB_HOST, UNSER, PASSWORD, DB_NAME);
- // $connnection = mysqli_connect( DB_HOST, UNSER, PASSWORD, DB_NAME, 8888); su pakeistu PORTu
 
- if( !$connnection ) {
-     echo "ERROR prisijungti prie DB " . DB_NAME . " nepavyko <br>";
-     echo "ERROR  " . mysqli_connect_error($connnection) . " <br>";
- } else {
-     echo "Jus prisijungete prie DB <br>";
+
+// getDoctor($nr)
+// createDoctor($vardas, $pavarde)
+// deleteDoctor($nr)
+// editDoctor($nr, $vardas, $pavarde)
+// getDoctors($kiekis = 9999)
+
+$connnection = mysqli_connect( DB_HOST, UNSER, PASSWORD, DB_NAME);
+// $connnection = mysqli_connect( DB_HOST, UNSER, PASSWORD, DB_NAME, 8888); su pakeistu PORTu
+
+function getPrisijungimas() {
+    global $connnection;
+
+    if( !$connnection ) {
+        echo "ERROR prisijungti prie DB " . DB_NAME . " nepavyko <br>";
+        echo "ERROR  " . mysqli_connect_error($connnection) . " <br>";
+    } else {
+        echo "Jus prisijungete prie DB <br>";
+    }
+
+    return $connnection;
+}
+
+ //===========
+ function getDoctor($nr) {
+     // global $connnection;
+     $manoSQL = "SELECT * FROM doctors WHERE id=2;";
+     $resultai = mysqli_query(    getPrisijungimas()  , $manoSQL); // mysqli_query - ivykdo uzduota SQL teksta
+
  }
-
+ $gydytojas = getDoctor(2); // masyvas
 
 //

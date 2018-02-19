c900b78dcf9e35038abe928e303a3c63e4aee788
Tautvydas <arttgd@gmail.com>
2018-02-19 08:27:00 +0200

    15d+

--- uzduotys/MAMP/diena_13/UZDUOTYS/php/1-uzduotis-php--user-admin-create_DB.php
+++
@@ -1,9 +0,0 @@
-// 0) sukurti DB "savaite4"
-// 1) use savaite4;             // pasirinkti lentele su kuria dirbsite
-// 2) sukurti lentele "users" su stulpeliais:
-// "id", "username", "pass", "email", "rights"
-// 2.2) sukurti pora nariu
-
-// 3) sukurti lentele "articles" su stulpeliais:
-// "id", "title", "content", "date", "user_id"
-// 3.2) sukurti pora straipsniu

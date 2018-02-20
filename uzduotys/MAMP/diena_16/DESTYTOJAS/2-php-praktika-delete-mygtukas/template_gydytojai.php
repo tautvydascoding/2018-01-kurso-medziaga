5535abddbe3fcf09a2dc3f75144e4e5183aa0059
grajauskas <e.grajauskas@gmail.com>
2018-02-20 16:52:37 +0200

    msf

--- uzduotys/MAMP/diena_16/DESTYTOJAS/2-php-praktika-delete-mygtukas/template_gydytojai.php
+++ uzduotys/MAMP/diena_16/DESTYTOJAS/2-php-praktika-delete-mygtukas/template_gydytojai.php
@@ -4,7 +4,7 @@
 <div class="container">
     <article class="row p-5">
         <?php
-            $gydytojas = getDoctor(9); //  Array ( [id] => 5 [name] => Koris [lname] => Pita )
+            $gydytojas = getDoctor(1); //  Array ( [id] => 5 [name] => Koris [lname] => Pita )
         ?>
         <div class="col-6 bg-info ">
             <h2>
@@ -18,5 +18,23 @@
             </h2>
         </div>
         <a href="delete_doctor.php?numeris=<?php   echo $gydytojas['id']; ?>" class="btn btn-danger "> DELETE </a>
+
+        <a href="#?trinti=true&numeris=<?php   echo $gydytojas['id']; ?>" class="btn btn-danger "> DELETE </a>
     </article>
 </div>
+
+
+
+
+<?php
+
+if( $_GET['trinti'] == true) {       // trinti=true
+    deleteDoctor( $_GET['numeris'] );//  numeris=9
+    // http://localhost/diena_16/DESTYTOJAS/2-php-praktika-delete-mygtukas/?trinti=true&numeris=9&action=delete
+
+    //&action=delete
+    //&action=edit
+    //&action=create
+}
+
+ ?>

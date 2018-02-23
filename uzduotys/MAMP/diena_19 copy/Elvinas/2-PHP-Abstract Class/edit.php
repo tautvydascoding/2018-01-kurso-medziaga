1af8f25a28c764a664debeade5d8f77e87887178
elvizz29 <35951774+elvizz29@users.noreply.github.com>
2018-02-23 15:55:07 +0200

    19 d.

---
+++ uzduotys/MAMP/diena_19 copy/Elvinas/2-PHP-Abstract Class/edit.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  reiklavimai - receptas - kokias f-jas privalo tureti klase, kuri naudoja interface Edit
+ */
+interface Edit
+{
+    public function changeName($a);
+    public function getName();
+    public function isLogin();
+}
